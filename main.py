5c7e54ca6df806e5987bc279ccf1fe42bda7d067
shilewenuw <shilewen1@gmail.com>
2021-11-17 10:14:29 -0800

    move configurables to top of file

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from utils import Subexchange, signed_get_request
 
 api_key = ''
 secret_key = ''
+subexchange = Subexchange.SPOT
+endpoint = '/sapi/v1/margin/account'
 
 def main():
-    print(signed_get_request(Subexchange.SPOT, '/sapi/v1/margin/account', api_key, secret_key))
+    response  = signed_get_request(subexchange, endpoint, api_key, secret_key)
+    print(f'Code: {response.status_code}')
+    print(f'Response: {response.text}')
 
 if __name__ == '__main__':
     main()
