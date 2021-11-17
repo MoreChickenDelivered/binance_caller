1e776a528b2d069d5c4ecb7172a33b29d98f4e0c
shilewenuw <shilewen1@gmail.com>
2021-11-17 10:20:55 -0800

    add example

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from utils import Subexchange, signed_get_request
+import json
 
 api_key = ''
 secret_key = ''
 subexchange = Subexchange.SPOT
 endpoint = '/sapi/v1/margin/account'
+# https://binance-docs.github.io/apidocs/spot/en/#query-cross-margin-account-details-user_data
+
+def process_response(response:str):
+    response = json.loads(response)
+    response['userAssets'] = {
+        assetInfo for assetInfo in response['userAssets']
+            if float(assetInfo['netAsset']) != 0
+    }
 
 def main():
     response  = signed_get_request(subexchange, endpoint, api_key, secret_key)
